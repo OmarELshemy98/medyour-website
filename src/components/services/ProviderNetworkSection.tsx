e40e77aa40ec41f8bb9099d8e33c1d2265711180
Omar Elshemy <omar.ibrahim@arconme.com>
2025-04-13 16:35:13 +0200

    add animation to provider-network in services page

--- src/components/services/ProviderNetworkSection.tsx
+++ src/components/services/ProviderNetworkSection.tsx
@@ -18,6 +18,16 @@ const ProviderNetworkSection = () => {
               priority
               sizes="(max-width: 768px) 100vw, (max-width: 1024px) 80vw, 75vw"
             />
+            {/* Animated squares */}
+            {/* Left side squares */}
+            <div className="absolute top-0 -left-2 w-5 h-5 bg-yellow-300 animate-float opacity-80"></div>
+            <div className="absolute top-1/2 -translate-y-10 -left-8 w-6 h-6 bg-[#00FF90] animate-float-delay opacity-80"></div>
+            <div className="absolute  left-8 w-4 h-4 bg-[#8CE2FF] animate-float blur-[2px] opacity-80"></div>
+            
+            {/* Right side squares */}
+            <div className="absolute top-15 -right-8 w-5 h-5 bg-[#FF00FB] animate-float-delay opacity-80"></div>
+            <div className="absolute top-2/3 -right-8 w-6 h-6 bg-[#8CE2FF] animate-float  opacity-80"></div>
+          
           </div>
         </div>
         <p className="text-[#00313B] text-center font-roboto text-[1rem] md:text-[1.125rem] font-normal leading-[1.25rem] md:leading-[1.5rem] max-w-2xl mx-auto px-4 md:px-0">
