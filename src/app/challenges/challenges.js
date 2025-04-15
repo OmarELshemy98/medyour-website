48bc09161d465535c62e2d06cf56540fdeb82441
Omar Elshemy <omar.ibrahim@arconme.com>
2025-04-15 19:17:07 +0200

    convert the website to static website

---
+++ src/app/challenges/challenges.js
@@ -0,0 +1,11 @@
+<script>
+{
+  anchor.addEventListener('click', function (e) {
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      e.preventDefault();
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
+</script>
